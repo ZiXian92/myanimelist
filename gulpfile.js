4f7848dd23af90b3384457782c69bd632a21ae89
zixian92 <zixian1992@hotmail.com>
2016-07-06 00:50:48 +0800

    Adds task to test compile react components

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const browserify = require('browserify');
 const nodemon = require('nodemon');
 const source = require('vinyl-source-stream');
 
-gulp.task('default', ['compile-clientjs', 'nodemon', 'watch']);
+gulp.task('default', ['compile-clientjs', 'test-react', 'nodemon', 'watch']);
 
 gulp.task('nodemon', function(){
   return nodemon({
     script: './server/server.js',
     ext: 'js jsx',
-    exec: 'babel-node',
+    exec: 'babel-node --debug',
     watch: ['./server/', './views/']
   }).on('start', function(){
     console.log('server started');
@@ -30,6 +30,15 @@ gulp.task('compile-clientjs', function(){
   .pipe(gulp.dest('./public'));
 });
 
+gulp.task('test-react', function(){
+  return browserify({
+    entries: './react-compile-test/app.jsx',
+    extensions: 'js jsx',
+    transform: [['babelify', {presets: ['react', 'es2015']}]],
+    debug: true
+  });
+});
+
 gulp.task('watch', function(){
-  return gulp.watch(['./views/**/*', './clientjs/*'], ['compile-clientjs']);
+  return gulp.watch(['./views/**/*', './clientjs/*'], ['compile-clientjs', 'test-react']);
 });
